0664d6f0bb535ef6bdaeefddee9dd382a526f3f7
Bang <belenmsr.805@gmail.com>
2025-09-23 15:17:38 -0400

    Creando Primera Vez

---
+++ main.js
@@ -0,0 +1,11 @@
+let numeroIngresado = 11;
+
+if (numeroIngresado > 10) {
+  console.log("Numero ingresado tiene dos digitos");
+} else {
+  if (numeroIngresado <= -10) {
+    console.log("Numero ingresado tiene dos digitos");
+  } else {
+    console.log("Numero ingresado tiene un digito");
+  }
+}
